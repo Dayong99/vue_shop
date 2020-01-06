e038296350567e46aa1eabc5b3337b1311368322
Dayong99 <2484030571@qq.com>
2020-01-06 12:25:14 +0800

    '登录/退出功能完成'

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+<!--    type="info"会将按钮渲染成灰色-->
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    methods: {
+      logout() {
+        window.sessionStorage.clear() // 清除window.sessionStorage里的token
+        this.$router.push('/login')  // 重定向到登陆界面
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 导入axios
+import axios from 'axios'
+// 配置请求的根路径
+// axios.defaults.baseURL = 'http://49.235.242.56:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 每一个vue组件都可以通过this直接访问到$http,从而发起axios请求
+Vue.prototype.axios = axios
 
 Vue.config.productionTip = false
 
